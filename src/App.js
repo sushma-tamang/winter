9c367ab65adcbbe6782646448da00a7e5e0428e2
sushma-tamang <ghisingsushma905@gmail.com>
2024-12-31 22:28:38 +0545

    change

--- src/App.js
+++ src/App.js
@@ -11,6 +11,10 @@ function App() {
       Jobs, internships and academic programs often require an application letter. You can also write an application letter to seek out unlisted opportunities and advertise yourself to potential employers. Your application letter is a description of why you're the ideal fit for the opening you're applying for, so it should be highly personalized. You can start writing by using a template then customizing it to suit your situation. In this article, we demonstrate how to write an application letter by sharing a variety of templates for professional situations.
 
       </p>
+      <p className='text-4xl text-gray-900 rounded-none bg-yellow-700 px-5 py-5 mx-3 my-3'>
+      1. Network Security Network security refers to measures taken to protect the integrity, confidentiality, and accessibility of data and resources in a computer network. ...
+
+      </p>
       <button className='text-3xl text-white px-4 py-4 rounded-3xl bg-yellow-950 text-justify'>
         submit
       </button>
