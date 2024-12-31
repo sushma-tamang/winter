6172bdfd7ec66e628a4db10a4727a3b901d09fc0
sushma-tamang <ghisingsushma905@gmail.com>
2024-12-31 22:21:47 +0545

    add

---
+++ src/App.js
@@ -0,0 +1,24 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='text-5xl text-white px-10 py-10 bg-black  text-center rounded-sm border-spacing-11 my-10'>
+        Head Application Letter
+      </h1>
+      <p className='text-2xl text-white bg-black px-8 py-8 my-5'>
+      Jobs, internships and academic programs often require an application letter. You can also write an application letter to seek out unlisted opportunities and advertise yourself to potential employers. Your application letter is a description of why you're the ideal fit for the opening you're applying for, so it should be highly personalized. You can start writing by using a template then customizing it to suit your situation. In this article, we demonstrate how to write an application letter by sharing a variety of templates for professional situations.
+
+      </p>
+      <button className='text-3xl text-white px-4 py-4 rounded-3xl bg-yellow-950 text-justify'>
+        submit
+      </button>
+      <button className='text-3xl text-white px-4 py-4 rounded-3xl bg-yellow-950 text-justify mx-5'>
+        cancel
+      </button>
+    </div>
+  );
+}
+
+export default App;
