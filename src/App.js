6584170615cbf8f75f1facd05437d6a151d8bce7
sushma-tamang <ghisingsushma905@gmail.com>
2025-01-02 07:41:58 +0545

    add

--- src/App.js
+++ src/App.js
@@ -8,19 +8,25 @@ function App() {
         Head Application Letter
       </h1>
       <p className='text-2xl text-white bg-black px-8 py-8 my-5'>
-      Jobs, internships and academic programs often require an application letter. You can also write an application letter to seek out unlisted opportunities and advertise yourself to potential employers. Your application letter is a description of why you're the ideal fit for the opening you're applying for, so it should be highly personalized. You can start writing by using a template then customizing it to suit your situation. In this article, we demonstrate how to write an application letter by sharing a variety of templates for professional situations.
+        Jobs, internships and academic programs often require an application letter. You can also write an application letter to seek out unlisted opportunities and advertise yourself to potential employers. Your application letter is a description of why you're the ideal fit for the opening you're applying for, so it should be highly personalized. You can start writing by using a template then customizing it to suit your situation. In this article, we demonstrate how to write an application letter by sharing a variety of templates for professional situations.
 
       </p>
       <p className='text-4xl text-gray-900 rounded-none bg-yellow-700 px-5 py-5 mx-3 my-3'>
-      1. Network Security Network security refers to measures taken to protect the integrity, confidentiality, and accessibility of data and resources in a computer network. ...
+        1. Network Security Network security refers to measures taken to protect the integrity, confidentiality, and accessibility of data and resources in a computer network. ...
 
       </p>
-      <button className='text-3xl text-white px-4 py-4 rounded-3xl bg-yellow-950 text-justify'>
-        submit
-      </button>
-      <button className='text-3xl text-white px-4 py-4 rounded-3xl bg-yellow-950 text-justify mx-5'>
-        cancel
-      </button>
+
+      <div className='flex justify-end'>
+
+
+
+
+        <button className='text-xl text-white px-10 py-2 rounded-md bg-yellow-950    hover:scale-105 hover:bg-yellow-400  duration-1000'>
+          submit
+        </button>
+        <button className='text-xl text-white px-10 py-2 rounded-md bg-yellow-950 flex justify-end mx-5'>
+          cancel
+        </button> </div>
     </div>
   );
 }
